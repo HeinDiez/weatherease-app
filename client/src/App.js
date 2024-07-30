3f7f78eba94b68134c878659571e292b174894c5
Hein Diez <heindiez@gmail.com>
2024-07-31 00:48:19 +0200

    Weatherease-app Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect } from "react";
+
+function App() {
+  const fetchData = async () => {
+    const res = await fetch("http://localhost:4000/");
+    console.log(res);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+  );
+}
+
+export default App;
