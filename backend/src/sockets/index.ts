68f01979f82f3e4b7d26f9b433629a312082fcc5
Hein Diez <heindiez@gmail.com>
2024-08-06 23:52:57 +0200

    update socket.io config.

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,25 +1,33 @@
 import { Server } from 'socket.io';
 import { handleWeatherSocket } from './weatherSocket';
 
+let io: Server | null = null;
+
 export const setupSocketIO = (server: any) => {
-    const io = new Server(server, {
-        cors: {
-            origin: process.env.WEATHEREASE_CLIENT_URL,
-            methods: ['GET', 'POST'],
-            credentials: true
-        },
-        transports: ['websocket', 'polling'],
-    });
-
-    io.on('connection', (socket) => {
-        console.log('New client connected');
-
-        handleWeatherSocket(socket);
-
-        socket.on('disconnect', () => {
-            console.log('Client disconnected');
+    if (!io) {
+        io = new Server(server, {
+            cors: {
+                origin: process.env.WEATHEREASE_CLIENT_URL,
+                methods: ['GET', 'POST'],
+                credentials: true
+            },
+            transports: ['websocket', 'polling'],
         });
-    });
+
+        io.on('connection', (socket) => {
+            console.log('New client connected');
+
+            handleWeatherSocket(socket);
+
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            });
+        });
+
+        console.log('Socket.IO server initialized');
+    } else {
+        console.log('Socket.IO server already running');
+    }
 
     return io;
 };
